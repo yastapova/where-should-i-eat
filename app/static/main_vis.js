9a2493b76e4bd3fce7235b8a9210ad8246e664f4
Yuliya Astapova <yva2@illinois.edu>
2019-08-06 23:30:40 -0400

    Finished concept map

--- app/static/main_vis.js
+++ app/static/main_vis.js
@@ -1,10 +1,12 @@
 var width = 1000;
-var height = 1000;
+var height = 800;
 
 var cuisines_file = "/static/data/test_cuisines.csv";
 var dishes_file = "/static/data/test_links.csv";
 
 var cuisine_id_map = {};
+var total_dishes_n = 0;
+var selected_cuisines = [];
 
 cuisines_temp = d3.csv(cuisines_file).then(function(cuisine_data) {
     for(var i = 0; i < cuisine_data.length; i++) {
@@ -65,6 +67,8 @@ cuisines_temp = d3.csv(cuisines_file).then(function(cuisine_data) {
             links: links
         }
 
+        total_dishes_n = data.outside.length;
+
         diameter = 960;
         rect_width = 140;
         rect_height = 14;
@@ -79,19 +83,20 @@ cuisines_temp = d3.csv(cuisines_file).then(function(cuisine_data) {
             .range([-(il * rect_height)/2, (il * rect_height)/2]);
 
         mid = (data.outside.length/2.0)
-        var outer_x = d3.scaleLinear()
-            .domain([0, mid, mid, data.outside.length])
-            .range([15, 170, 190 ,355]);
-
-        var outer_y = d3.scaleLinear()
+        var outer_x = d3.scaleQuantile()
             .domain([0, data.outside.length])
-            .range([0, diameter / 2 - 120]);
+            .range([-350, 350]);
+
+        var chart_height = height / 2
+        var outer_y = d3.scaleLinear()
+            .domain([0, Math.ceil(total_dishes_n / 2)])
+            .range([-chart_height + 50, chart_height + 50]);
 
 
         // setup positioning
         data.outside = data.outside.map(function(d, i) {
             d.x = outer_x(i);
-            d.y = diameter/3;
+            d.y = outer_y(i % (total_dishes_n / 2));
             return d;
         });
 
@@ -111,11 +116,9 @@ cuisines_temp = d3.csv(cuisines_file).then(function(cuisine_data) {
             return deg;
         }
 
-        var link_generator = d3.linkRadial()
-            .source(function(d) { return [projectTheta(d.inner.x, d.inner.y + rect_height/2),
-                                          d.outer.x > 180 ? projectR(d.inner.x, d.inner.y) : projectR(d.inner.x + rect_width, d.inner.y)]; })
-            .target(function(d) { return [projectTheta(d.outer.x, d.outer.y),
-                                          projectR(d.outer.x, d.outer.y)]; });
+        var link_generator = d3.linkHorizontal()
+            .source(function(d) { return [d.outer.x < 0 ? d.inner.x : d.inner.x + rect_width, (d.inner.y + rect_height/2)]; })
+            .target(function(d, i) { return [d.outer.x, d.outer.y] });
 
         var svg = d3.select("svg")
             .attr("width", width)
@@ -130,18 +133,14 @@ cuisines_temp = d3.csv(cuisines_file).then(function(cuisine_data) {
           .enter().append('path')
             .attr('class', 'link')
             .attr('id', function(d) { return d.id })
-            .attr("d", function(d) { return link_generator(d); })
-            .attr('stroke', "black")
-            .attr('stroke-width', link_width)
-            .style("fill", "none");
+            .attr("d", function(d) { return link_generator(d); });
 
         // outer nodes
-
         var onode = svg.append('g').selectAll(".outer_node")
             .data(data.outside)
           .enter().append("g")
             .attr("class", "outer_node")
-            .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+            .attr("transform", function(d, i) { return "translate(" + d.x + ", " + d.y + ")"; })
             .on("mouseover", mouseover)
             .on("mouseout", mouseout);
 
@@ -156,8 +155,8 @@ cuisines_temp = d3.csv(cuisines_file).then(function(cuisine_data) {
         onode.append("text")
             .attr('id', function(d) { return d.id + '-txt'; })
             .attr("dy", ".31em")
-            .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-            .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+            .attr("text-anchor", function(d) { return d.x > 0 ? "start" : "end"; })
+            .attr("transform", function(d) { return d.x > 0 ? "translate(8)" : "translate(-8)"; })
             .text(function(d) { return d.name; });
 
         // inner nodes
@@ -168,13 +167,19 @@ cuisines_temp = d3.csv(cuisines_file).then(function(cuisine_data) {
             .attr("class", "inner_node")
             .attr("transform", function(d, i) { return "translate(" + d.x + "," + d.y + ")"})
             .on("mouseover", mouseover)
-            .on("mouseout", mouseout);
+            .on("mouseout", mouseout)
+            .on("click", function(d) {
+                if(d3.select("#" + d.id).classed("selected")) {
+                    click_unselect(d);
+                } else {
+                    click_select(d);
+                }
+            });
 
         inode.append('rect')
             .attr('width', rect_width)
             .attr('height', rect_height)
-            .attr('id', function(d) { return d.id; })
-            .attr('fill', "limegreen");
+            .attr('id', function(d) { return d.id; });
 
         inode.append("text")
             .attr('id', function(d) { return d.id + '-txt'; })
@@ -186,36 +191,68 @@ cuisines_temp = d3.csv(cuisines_file).then(function(cuisine_data) {
 
 //        d3.select(self.frameElement).style("height", diameter - 150 + "px");
 
-        function mouseover(d)
-        {
+        function mouseover(d) {
             // bring to front
-//            d3.selectAll('.links .link').sort(function(a, b){ return d.related_links.indexOf(a.id); });
-//
-//            for (var i = 0; i < d.related_nodes.length; i++)
-//            {
-//                d3.select('#' + d.related_nodes[i]).classed('highlight', true);
-//                d3.select('#' + d.related_nodes[i] + '-txt').attr("font-weight", 'bold');
-//            }
-//
-//            for (var i = 0; i < d.related_links.length; i++)
-//                d3.select('#' + d.related_links[i]).attr('stroke-width', '5px');
+            d3.selectAll('.links .link').sort(function(a, b){ return d.related_links.indexOf(a.id); });
+
+            for (var i = 0; i < d.related_nodes.length; i++) {
+                d3.select('#' + d.related_nodes[i]).classed('highlight', true);
+                d3.select('#' + d.related_nodes[i] + '-txt').classed('highlight', true);
+            }
+
+            for (var i = 0; i < d.related_links.length; i++) {
+                d3.select('#' + d.related_links[i]).classed('highlight', true);
+            }
             return;
         }
 
-        function mouseout(d)
-        {
-//            for (var i = 0; i < d.related_nodes.length; i++)
-//            {
-//                d3.select('#' + d.related_nodes[i]).classed('highlight', false);
-//                d3.select('#' + d.related_nodes[i] + '-txt').attr("font-weight", 'normal');
-//            }
-//
-//            for (var i = 0; i < d.related_links.length; i++)
-//                d3.select('#' + d.related_links[i]).attr('stroke-width', link_width);
+        function mouseout(d) {
+            for (var i = 0; i < d.related_nodes.length; i++) {
+                d3.select('#' + d.related_nodes[i]).classed('highlight', false);
+                d3.select('#' + d.related_nodes[i] + '-txt').classed('highlight', false);
+            }
+
+            for (var i = 0; i < d.related_links.length; i++) {
+                d3.select('#' + d.related_links[i]).classed('highlight', false);
+            }
+
+            return;
+        }
+
+        function click_select(d) {
+            d3.selectAll('.links .link').sort(function(a, b){ return d.related_links.indexOf(a.id); });
+
+            for (var i = 0; i < d.related_nodes.length; i++) {
+                d3.select('#' + d.related_nodes[i]).classed('selected', true);
+                d3.select('#' + d.related_nodes[i] + '-txt').classed('selected', true);
+            }
+
+            for (var i = 0; i < d.related_links.length; i++) {
+                d3.select('#' + d.related_links[i]).classed('selected', true);
+            }
+
+            selected_cuisines.push(d.id);
+
             return;
         }
 
 
+        function click_unselect(d) {
+            d3.selectAll('.links .link').sort(function(a, b){ return d.related_links.indexOf(a.id); });
+
+            for (var i = 0; i < d.related_nodes.length; i++) {
+                d3.select('#' + d.related_nodes[i]).classed('selected', false);
+                d3.select('#' + d.related_nodes[i] + '-txt').classed('selected', false);
+            }
+
+            for (var i = 0; i < d.related_links.length; i++) {
+                d3.select('#' + d.related_links[i]).classed('selected', false);
+            }
+
+            selected_cuisines.splice(selected_cuisines.indexOf(d.id), 1);
+
+            return;
+        }
 
     });
 });
